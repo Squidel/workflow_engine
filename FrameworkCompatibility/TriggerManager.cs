a5b990892d8b83aa2d18da79d071dad1a92e40ea
Tarique Solomon <tarique.solomon@jsif.org>
2025-01-13 08:56:52 -0500

    feat: add "logs" to the trigger mechanism

--- FrameworkCompatibility/TriggerManager.cs
+++ FrameworkCompatibility/TriggerManager.cs
@@ -10,21 +10,30 @@ namespace FrameworkCompatibility
     public class TriggerManager<T>
     {
         private readonly List<Trigger<T>> _triggers = new List<Trigger<T>>();
+        private readonly List<string> _logs = new List<string>();
 
         public TriggerManager<T> AddTrigger(Func<T, bool> condition, Action<T> action)
         {
             _triggers.Add(new Trigger<T>(condition, action));
             return this;
         }
-        public void EvaluateTriggers(T obj)
+        public TriggerManager<T> EvaluateTriggers(T obj)
         {
+            StringBuilder logMessage = new StringBuilder();
             foreach (var trigger in _triggers)
             {
+                logMessage.AppendLine($"About to evaluate condition: {trigger.Condition.Method.Name}");
                 if (trigger.Condition(obj))
                 {
+                    logMessage.AppendLine($"  | trigger condition evaluated to true; about to execute {trigger.Action.Method.Name}"_;
                     trigger.Action(obj);
+                    logMessage.AppendLine($"  | executed {trigger.Action.Method.Name}");
                 }
+                _logs.Add(logMessage.ToString());
             }
+            return this;
         }
+
+        public List<string> GetLogs() => _logs;
     }
 }
