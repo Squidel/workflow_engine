e40f0dc2d4d4cee5bfc6697c882c82fbf2b24de3
Tarique Solomon <tarique.solomon@jsif.org>
2025-01-13 08:39:24 -0500

    feat: added triggers to project. Allows conditions to be defined and performs actions based on whether the conditions evaluate to true. Aides in ensuring cleaner code and not much repetitions in code. Should encourage a set structure to classes

---
+++ FrameworkCompatibility/Trigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkCompatibility
+{
+    public class Trigger<T>
+    {
+        public Func<T, bool>Condition { get; }
+        public Action<T> Action { get; }
+        public Trigger(Func<T, bool> condition, Action<T> action)
+        {
+            this.Condition = condition??throw new ArgumentNullException(nameof(condition));
+            this.Action = action ?? throw new ArgumentNullException(nameof(action));
+        }
+    }
+}
---
+++ FrameworkCompatibility/TriggerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkCompatibility
+{
+    public class TriggerManager<T>
+    {
+        private readonly List<Trigger<T>> _triggers = new List<Trigger<T>>();
+
+        public TriggerManager<T> AddTrigger(Func<T, bool> condition, Action<T> action)
+        {
+            _triggers.Add(new Trigger<T>(condition, action));
+            return this;
+        }
+        public void EvaluateTriggers(T obj)
+        {
+            foreach (var trigger in _triggers)
+            {
+                if (trigger.Condition(obj))
+                {
+                    trigger.Action(obj);
+                }
+            }
+        }
+    }
+}
